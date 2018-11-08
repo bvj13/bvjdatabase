136736bce935b12fed5cf627ec0568d238e29d3d
brendan.vanjacobs <brendan.vanjacobs@schwab.com>
2018-11-07 21:53:33 -0700

    BVJ updated inset

--- Database1/Database1/PostDeploy/emp_table_insert.sql
+++ Database1/Database1/PostDeploy/emp_table_insert.sql
@@ -1,42 +1,42 @@
 ﻿INSERT INTO EMP VALUES
         (7369, 'SMITH',  'CLERK',     7902,
-        '12/17/1980', 800, NULL, 20)
+        '12/17/1980', 800, NULL, 20, 2)
 INSERT INTO EMP VALUES
         (7499, 'ALLEN',  'SALESMAN',  7698,
-        '2/20/1981', 1600,  300, 30)
+        '2/20/1981', 1600,  300, 3, 2)
 INSERT INTO EMP VALUES
         (7521, 'WARD',   'SALESMAN',  7698,
-        '2/22/1981', 1250,  500, 30)
+        '2/22/1981', 1250,  500, 3, 2)
 INSERT INTO EMP VALUES
         (7566, 'JONES',  'MANAGER',   7839,
-        '4/2/1981',  2975, NULL, 20)
+        '4/2/1981',  2975, NULL, 2, 2)
 INSERT INTO EMP VALUES
         (7654, 'MARTIN', 'SALESMAN',  7698,
-        '9/28/1981', 1250, 1400, 30)
+        '9/28/1981', 1250, 1400, 3, 2)
 INSERT INTO EMP VALUES
         (7698, 'BLAKE',  'MANAGER',   7839,
-        '5/1/1981',  2850, NULL, 30)
+        '5/1/1981',  2850, NULL, 3, 2)
 INSERT INTO EMP VALUES
         (7782, 'CLARK',  'MANAGER',   7839,
-        '6/9/1981',  2450, NULL, 10)
+        '6/9/1981',  2450, NULL, 1, 2)
 INSERT INTO EMP VALUES
         (7788, 'SCOTT',  'ANALYST',   7566,
-        '12/9/1982', 3000, NULL, 20)
+        '12/9/1982', 3000, NULL, 2, 2)
 INSERT INTO EMP VALUES
         (7839, 'KING',   'PRESIDENT', NULL,
-        '11/17/1981', 5000, NULL, 10)
+        '11/17/1981', 5000, NULL, 1, 2)
 INSERT INTO EMP VALUES
         (7844, 'TURNER', 'SALESMAN',  7698,
-        '9/8/1981',  1500,    0, 30)
+        '9/8/1981',  1500,    0, 3, 2)
 INSERT INTO EMP VALUES
         (7876, 'ADAMS',  'CLERK',     7788,
-        '1/12/1983',1100, NULL, 20)
+        '1/12/1983',1100, NULL, 2, 2)
 INSERT INTO EMP VALUES
         (7900, 'JAMES',  'CLERK',     7698,
-        '12/3/1981', 950, NULL, 30)
+        '12/3/1981', 950, NULL, 3, 2)
 INSERT INTO EMP VALUES
         (7902, 'FORD',   'ANALYST',   7566,
-        '12/3/1981', 3000, NULL, 20)
+        '12/3/1981', 3000, NULL, 2, 2)
 INSERT INTO EMP VALUES
         (7934, 'MILLER', 'CLERK',     7782,
-        '1/23/1982', 1300, NULL, 10)
+        '1/23/1982', 1300, NULL, 1, 2)
